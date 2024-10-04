1b669f5465fb5f56fee46086d29b590c735f4d9a
josh-espinas <josh.espinas@xero.com>
2024-10-04 12:10:06 +0100

    Set up check for an initialised DB

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@
 mysqld_safe &
 sleep 10
 
+if [ ! -d "/var/lib/mysql/my_database" ]; then
+  echo "No database has been initialised, performing initilisation steps now..."
+  echo "Placeholder command"
+fi
+
 # To keep the container running
 tail -f /dev/null
 
